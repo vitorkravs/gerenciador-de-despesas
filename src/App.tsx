573e191af614d83503bcb7edd70e071ce964bb1c
vitor kravszenko <vitorkravs3@gmail.com>
2023-12-01 11:52:45 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from "./App.styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: any = () => {
+  <C.Container>
+    <C.Header>
+      <C.HeaderText>Hello world</C.HeaderText>
+    </C.Header>
+  </C.Container>;
+};
 
 export default App;
